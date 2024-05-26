e04830cd1ce9a235d2da14915afef60b8dca6bcf
Yanniii02 <Jsalvamante@gbox.adnu.edu.ph>
2024-05-26 19:39:26 +0800

    4th Commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_26_113419) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_26_113753) do
   create_table "authors", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email_address"
@@ -23,6 +23,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_26_113419) do
     t.string "name"
     t.datetime "publication_date"
     t.integer "edition"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "libraries", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
